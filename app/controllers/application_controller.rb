00f61320b8bacd381457dc28799dba9de3ecbe94
cforee <cforee@gmail.com>
2019-08-28 23:19:55 -0700

    Add default route;  Update gemfile to include Devise token auth gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :null_session
+  before_action :set_client
+
+  def render_json(status, data=nil)
+    render(json: data, status: status)
+  end
+
+  def status
+    render_json(:ok, { success: true })
+  end
+
+  private
+
+  def set_client
+    # find an extant User object
+    # to define the current user
+  end
+
 end
